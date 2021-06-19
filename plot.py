50f5c22f1b4a69ac748c0a76600b5670c75ec7d7
czechen <czechen@protonmail.com>
2021-06-19 22:40:38 +0200

    small progress

--- plot.py
+++ plot.py
@@ -4,15 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os.path
 
-filename = "out.txt"
-if not os.path.isfile(filename):
+filename1 = "out1.txt"
+if not os.path.isfile(filename1):
     print('File does not exist.')
 else:
-    f = open(filename)
+    f1 = open(filename1)
         
 
 
-positions = [data.strip() for line in f.readlines() for data in line.split(',') if data.strip()]
+positions1 = [data.strip() for line in f1.readlines() for data in line.split(',') if data.strip()]
 rx1 = []
 ry1 = []
 rx2 = []
@@ -20,23 +20,62 @@ ry2 = []
 rxp = []
 ryp = []
 i = 0
-while (i < len(positions)):
-    rx1.append(float(positions[i]))
-    ry1.append(float(positions[i+1]))
-    rx2.append(float(positions[i+2]))
-    ry2.append(float(positions[i+3]))
-    rxp.append(float(positions[i+4]))
-    ryp.append(float(positions[i+5]))
+while (i < len(positions1)):
+    rx1.append(float(positions1[i]))
+    ry1.append(float(positions1[i+1]))
+    rx2.append(float(positions1[i+2]))
+    ry2.append(float(positions1[i+3]))
+    rxp.append(float(positions1[i+4]))
+    ryp.append(float(positions1[i+5]))
     i += 6
 
-fig1, ax = plt.subplots()
-ax.set_box_aspect(1)
-ax.scatter(rx1,ry1,s=100,c='#FD2E24')
-ax.scatter(rx2,ry2,s=5,c='#2E1A81')
-ax.scatter(rxp,ryp,s=1,c='black')
+fig1, ax1 = plt.subplots()
+ax1.set_box_aspect(1)
+ax1.scatter(rx1,ry1,s=100,c='#FD2E24')
+ax1.scatter(rx2,ry2,s=5,c='#2E1A81')
+ax1.scatter(rxp,ryp,s=1,c='black')
 plt.xlabel('x')
 plt.ylabel('y')
-ax.legend(['sun','earth','particle'])
+ax1.legend(['sun','earth','particle'])
 
 plt.show()
 
+
+filename2 = "out2.txt"
+if not os.path.isfile(filename2):
+    print('File does not exist.')
+else:
+    f2 = open(filename2)
+        
+
+
+positions2 = [data.strip() for line in f2.readlines() for data in line.split(',') if data.strip()]
+rxCM =0
+ryCM =0
+uxp = []
+uyp = []
+#print(positions2)
+i = 0
+while (i < len(positions2)):
+    if i == 0:
+        ux1 = float(positions2[i])
+        uy1 = float(positions2[i+1])
+        ux2 = float(positions2[i+2])
+        uy2 = float(positions2[i+3])
+        i += 4
+    else:
+        uxp.append(float(positions2[i]))
+        uyp.append(float(positions2[i+1]))
+        i += 2
+
+fig2, ax2 = plt.subplots()
+ax2.set_box_aspect(1)
+ax2.scatter(rxCM,ryCM,s=10,c='black')
+ax2.scatter(ux1,uy1,s=100,c='#FD2E24')
+ax2.scatter(ux2,uy2,s=5,c='#2E1A81')
+ax2.scatter(uxp,uyp,s=1,c='black')
+plt.xlabel('x')
+plt.ylabel('y')
+ax2.legend(['center of mass','sun','earth','particle'])
+
+plt.show()
